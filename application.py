5a413cb73854a20b0b007fd4e5d1cbda92ab9fcc
NirBZ <nirey10@gmail.com>
2022-12-20 15:19:01 +0200

    added third design

--- application.py
+++ application.py
@@ -7,7 +7,7 @@ def hello_world():
     ynet_str = get_ynet_articles()
     one_sport_str = get_one_articles()
     globs_str = get_globs_articles()
-    return render_template('./screen1/lobby_screen1.html', ynet_titles_str=ynet_str, globs_titles_str=globs_str, one_titles_str=one_sport_str)
+    return render_template('./screen1/lobby_screen.html', ynet_titles_str=ynet_str, globs_titles_str=globs_str, one_titles_str=one_sport_str)
 
 if __name__ == '__main__':
    application.run()
